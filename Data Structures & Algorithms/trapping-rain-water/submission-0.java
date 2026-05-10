467fe7ad53524f68ddb7fa1fad1dc7edfe897076
Steven Carmona <scarmona1030@gmail.com>
2026-05-10 00:00:39 -0700

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int trap(int[] height) {
+        if(height.length == 0) {
+            return 0;
+        }
+        int res = 0;
+
+        // pointers
+        int l = 0;
+        int r = height.length-1;
+
+        // max
+        int maxLeft = height[l];
+        int maxRight = height[r];
+
+        while(l < r) {
+
+            if(maxLeft < maxRight) {
+                l++;
+                maxLeft = Math.max(maxLeft, height[l]);
+                res += maxLeft - height[l];
+            } else {
+                r--;
+                maxRight = Math.max(maxRight, height[r]);
+                res += maxRight - height[r];
+            }
+            
+        }
+
+        return res; 
+    }
+}
