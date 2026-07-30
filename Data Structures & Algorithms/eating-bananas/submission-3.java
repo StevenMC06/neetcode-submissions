b2577eb4fdd0ff6d3ecac4d0abf962448da973de
Steven Carmona <scarmona1030@gmail.com>
2026-07-29 18:29:08 -0700

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 0;
+
+        for(int pile: piles) {
+            right = Math.max(pile, right);
+        }
+
+        int res = right;
+
+        while(left <= right) {
+            int k = (left + right)/2;
+            int hours = 0;
+
+            for(int i = 0; i < piles.length; i++) {
+                hours += Math.ceil((double)piles[i]/k);
+            }
+
+            if(hours <= h) {
+                res = k;
+                right = k-1;
+            } else {
+                left = k+1;
+            }
+            
+        }
+
+        return res;
+    }
+}
